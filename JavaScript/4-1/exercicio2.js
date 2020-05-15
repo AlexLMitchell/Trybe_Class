c2cf96247de1cdcc9f191efb19aa250630ad8a81
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-05-15 18:52:23 -0300

    ex 4 e 5

--- JavaScript/4-1/exercicio2.js
+++ JavaScript/4-1/exercicio2.js
@@ -1,2 +1,5 @@
-Faça um programa que retorne o maior de dois números.
-Defina no começo do programa duas variáveis com os valores que serão comparados.
+let a = 12;
+let b = 8;
+
+if (a>b) console.log("a é o maior numero");
+else console.log("b é o maior numero");
--- JavaScript/4-1/exercicio4.js
+++ JavaScript/4-1/exercicio4.js
@@ -1,4 +1,4 @@
-Faça um programa que, dado um valor definido numa variável,
-retorne “positive” se esse valor for positivo, “negative”
-se for negativo e “zero” caso contrário.
+let a = 2;
 
+if(a > 0) console.log("Positive");
+else console.log("negative");
